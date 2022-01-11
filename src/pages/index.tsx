a86c93125c5dc561e18dd7fc5b24f15afcfc79f0
narendracode <narendra.kumar.soni28@gmail.com>
2022-01-11 18:55:33 +0800

    code refactored and zilpay connect added to nfts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,6 @@ const checkIfWalletIsConnected = async () => {
       }
       try {
           if (typeof zilPay !== "undefined" && zilPay.wallet.isConnect) {
-              const result = zilPay.wallet.defaultAccount;
               const { bech32, base16 } = zilPay.wallet.defaultAccount;
               dispatch(setWallet({
                   bech32: bech32,
@@ -64,6 +63,34 @@ const checkIfWalletIsConnected = async () => {
     checkIfWalletIsConnected()
   }, [])
   
+  const renderZilpayConnectView = () => {
+    return (
+      <div className="flex flex-row">
+        <div className="basis-1/4 md:basis-1/3">
+          <button className="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded" onClick={connect}>
+            {`Connect Your Zilpay Wallet`}
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  const renderConnectedWalletView = () => {
+    return (
+      <div className="mt-3 flex space-x-2">
+      <span> 
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+        </svg> 
+      </span>
+      <span className="font-mono text-xl">
+        Wallet connected 
+      </span> 
+      <span className="font-mono text-xl text-indigo-700"> {walletInfo.base16}</span>
+    </div>
+    )
+  }
+
   return (
     <>
       <div className="">
@@ -73,28 +100,12 @@ const checkIfWalletIsConnected = async () => {
       </div>
       {
       walletInfo.bech32 === '' && walletInfo.base16 === '' && 
-      <div className="flex flex-row">
-        <div className="basis-1/4 md:basis-1/3">
-          <button className="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded" onClick={connect}>
-            {`Connect Your Zilpay Wallet`}
-          </button>
-        </div>
-      </div>
+        renderZilpayConnectView()
       }
            
       {
         walletInfo.bech32 !== '' && walletInfo.base16 !== '' &&
-          <div className="mt-3 flex space-x-2">
-            <span> 
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-              </svg> 
-            </span>
-            <span className="font-mono text-xl">
-              Wallet connected 
-            </span> 
-            <span className="font-mono text-xl text-indigo-700"> {walletInfo.base16}</span>
-          </div>
+        renderConnectedWalletView()
       }
     </>
   )
--- src/pages/nfts.tsx
+++ src/pages/nfts.tsx
@@ -1,4 +1,54 @@
+import { useEffect, useState } from "react";
+import { delay} from '../utils/wallet'
+import { useAppDispatch } from "../redux/hooks";
+import { setWallet } from '../redux/features/wallet'
+
 export default function Nfts() {
+    const dispatch = useAppDispatch();
+    const [walletInfo, setWalletInfo] = useState(null);
+
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            let zilPay = (window as any).zilPay;
+            if (!zilPay) {
+                await delay(1500) // wallet injection may sometimes be slow
+                zilPay = (window as any).zilPay;
+            }
+            try {
+                if (typeof zilPay !== "undefined" && zilPay.wallet.isConnect) {
+                    const { bech32, base16 } = zilPay.wallet.defaultAccount;
+                    dispatch(setWallet({
+                        bech32: bech32,
+                        base16: base16
+                    }))
+                    setWalletInfo({
+                        bech32: bech32,
+                        base16: base16
+                    })
+                }
+            } catch (e) {
+                console.error(e);
+            }
+        } catch (error) {
+            console.log(`error while checking zilpay wallet.`)
+        }
+      }
+        useEffect( () => {
+          checkIfWalletIsConnected()
+        }, [])
+
+
+ useEffect( () => {
+    console.log(`wallet : ${JSON.stringify(walletInfo)}`)
+    if(walletInfo && walletInfo.base16 && walletInfo.bech32){
+        console.log(`wallet info found, fetch NFTs from this wallet.`);
+    }else{
+        console.log(`Wallet not connected yet by this user.`)
+        return;
+    }
+  }, [walletInfo])
+
  return (
     <div>
         NFTs
